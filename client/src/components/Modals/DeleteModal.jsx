959fcb3759ec562e6642a9f6fed49df32e76d491
Nícolas Kormann <nicolas.kormann@proton.me>
2024-01-10 15:47:58 -0300

    style: update button color in DeleteModal

--- client/src/components/Modals/DeleteModal.jsx
+++ client/src/components/Modals/DeleteModal.jsx
@@ -65,14 +65,14 @@ const DeleteModal = ({ open, setOpen, confirmDelete }) => {
                 <div className="bg-slate-900 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
                   <button
                     type="button"
-                    className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
+                    className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-700 sm:ml-3 sm:w-auto"
                     onClick={() => confirmDelete()}
                   >
                     Excluir
                   </button>
                   <button
                     type="button"
-                    className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+                    className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-white/95 sm:mt-0 sm:w-auto"
                     onClick={() => setOpen(false)}
                     ref={cancelButtonRef}
                   >
